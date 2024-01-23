85e4aee610a7506ecc188fb1f336be05b5bb6877
Jeff Wallsinger <jeffwall97@users.noreply.github.com>
2024-01-23 16:38:02 -0500

    added create invoice capability

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -13,7 +13,7 @@ export function CreateInvoice() {
   );
 }
 
-export function UpdateInvoice({ id }: { id: string }) {
+export function UpdateInvoice({ id }: Readonly<{ id: string }>) {
   return (
     <Link
       href="/dashboard/invoices"
@@ -24,13 +24,11 @@ export function UpdateInvoice({ id }: { id: string }) {
   );
 }
 
-export function DeleteInvoice({ id }: { id: string }) {
+export function DeleteInvoice({ id }: Readonly<{ id: string }>) {
   return (
-    <>
-      <button className="rounded-md border p-2 hover:bg-gray-100">
-        <span className="sr-only">Delete</span>
-        <TrashIcon className="w-5" />
-      </button>
-    </>
+    <button className="rounded-md border p-2 hover:bg-gray-100">
+      <span className="sr-only">Delete</span>
+      <TrashIcon className="w-5" />
+    </button>
   );
 }
